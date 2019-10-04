d620492074d2aec90403011762b3ea78a2359efe
Raphael Stäbler <raphael@productionbuild.de>
2019-10-04 21:08:59 +0200

    Added additional comments

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    // Light up internal LED
+    // Configure GPIO B0_03 (PIN 13) for output
     IOMUXC_SW_MUX_CTL_PAD_GPIO_B0_03 = 5;
     IOMUXC_SW_PAD_CTL_PAD_GPIO_B0_03 = IOMUXC_PAD_DSE(7);
     IOMUXC_GPR_GPR27 = 0xFFFFFFFF;
@@ -11,16 +11,20 @@ int main()
     for(;;) {
         volatile unsigned int i = 0;
 
+        // Set PIN 13 HIGH
         GPIO7_DR_SET = (1 << 3);
 
+        // Poor man's delay
         while(i < 10000000) {
             i++;
         }
 
         i = 0;
 
+        // Set PIN 13 LOW
         GPIO7_DR_CLEAR = (1 << 3);
 
+        // Poor man's delay
         while(i < 10000000) {
             i++;
         }
